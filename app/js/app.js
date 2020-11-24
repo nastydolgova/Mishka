38bbf726e644b7669802616c12c7c918fa68c323
Anastasia D <nasty.dolgova@gmail.com>
2020-11-24 13:31:33 +0300

    change main nav

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
 let menuBtn = document.querySelector('.main-nav__toogle');
-let mainNav = document.querySelector('.main-nav__wrapper');
+let mainNavWr = document.querySelector('.main-nav__wrapper');
 let bgDiv = document.querySelector('.body__background');
+let mainNav = document.querySelector('.main-nav');
 
 menuBtn.addEventListener('click', openMenu );
+mainNav.classList.remove('main-nav--no-js');
 
 function openMenu(){
-    mainNav.classList.toggle('hidden');
+    mainNav.classList.toggle('main-nav--closed');
     bgDiv.classList.toggle('hidden');
     menuBtn.classList.toggle('main-nav__toogle--close');
 }
