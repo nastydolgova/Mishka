e22f465a8bc4dea1a7db72b1a28fa62d718bf8d5
Anastasia D <nasty.dolgova@gmail.com>
2020-12-11 13:39:04 +0300

    webp

---
+++ app/js/form.js
@@ -0,0 +1,72 @@
+'use strict';
+(function(){
+    // let form = document.querySelector('.form');
+    let formBtn = document.querySelector('.form__button');
+    let inputs = document.querySelectorAll('.form__input');
+
+    let orders = [];
+    
+    formBtn.addEventListener('click', addOrder);
+
+
+    // for (let i = 0; i < inputs.length; i++) {
+    //   inputs[i].addEventListener("invalid", function (evt) {
+    //     if (!inputs[i].valid) {
+    //       inputs[i].setCustomValidity("Заполните поле верно");
+    //       smbtBtn.addEventListener("click", function () {
+    //         inputs[i].classList.add("invalid-info");
+    //       });
+    //     }
+    //   });
+    // }
+    
+    function addOrder(e){
+        e.preventDefault();
+        let newOrder = new Order('Type', 'Color', 'Name', 'Surname', 'MiddleName', 'Phone', 'Mail', 'Comment')
+        orders.push(newOrder)
+        alert("Заказ успешно отправлен в консоль!");
+        console.log(newOrder)
+        console.log(orders)
+    }
+
+    class Order {
+        constructor (type, color, name, surname, middleName, phone, mail, comment){
+            this.Type = this._check(type)
+            this.Color = this._getArray(color);
+            this.Name = this._text(name);
+            this.Surname = this._text(surname);
+            this.MiddleName = this._text(middleName);
+            this.Phone = this._text(phone);
+            this.Mail = this._text(mail);
+            this.Comment = this._textArea(comment);
+        }
+        _check(attrName) {
+            let obj = document.querySelector(`input[name=${attrName}]:checked`);
+            return obj.value;
+          }
+
+          _getArray(attrName) {
+            let objArr = [
+              ...document.querySelectorAll(`input[name=${attrName}]:checked`),
+            ];
+            let arr = [];
+            objArr.forEach((el) => {
+              arr.push(el.value);
+            });
+            return arr;
+          }
+
+          _text(attrName) {
+            let obj = document.querySelector(`input[name=${attrName}]`);
+            return obj.value;
+          }
+         
+          _textArea(attrName) {
+            let obj = document.querySelector(`textarea[name=${attrName}]`);
+            return obj.value;
+          }
+    }
+
+})();
+
+
---
+++ app/js/openMenu.js
@@ -0,0 +1,18 @@
+'use strict';
+(function(){
+let mainNav = document.querySelector('.main-nav');
+let menuBtn = document.querySelector('.main-nav__toogle');
+let bgDiv = document.querySelector('.body__background');
+
+mainNav.classList.remove('main-nav--no-js');
+
+// Открытие меню
+
+menuBtn.addEventListener('click', openMenu );
+
+function openMenu(){
+    mainNav.classList.toggle('main-nav--closed');
+    bgDiv.classList.toggle('hidden');
+    menuBtn.classList.toggle('main-nav__toogle--close');
+}
+})();
